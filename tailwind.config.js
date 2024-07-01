58da7043ff651c44a7ef1f9957dd3bc8452781ba
Haris Subhan <jackieliam7@gmail.com>
2024-07-01 21:29:33 +0500

    complete

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#151C2D",
+        secondary: "#192237",
+      },
+    },
+  },
+  plugins: [],
+};
